dfc6592ff51b8944e1fd38486b97f65386ecb18b
codepgq <devpgq@126.com>
2018-06-11 10:02:18 +0800

    string add medtod data/bytes

--- PQTool/PQTool/Extensions/PQString.swift
+++ PQTool/PQTool/Extensions/PQString.swift
@@ -189,6 +189,51 @@ public extension PQStringEncodable where WrapperType == String {
         }
         return false
     }
+    
+    //将十六进制字符串转化为 Data
+    func data() -> Data {
+        let bytes = self.bytes()
+        return Data(bytes: bytes)
+    }
+    
+    // 将16进制字符串转化为 [UInt8]
+    // 使用的时候直接初始化出 Data
+    // Data(bytes: Array<UInt8>)
+    func bytes() -> [UInt8] {
+        var hexStr = ""
+        pq.components(separatedBy: " ").forEach({ hexStr.append($0) })
+        
+        assert(hexStr.count % 2 == 0, "输入字符串格式不对，8位代表一个字符")
+        var bytes = [UInt8]()
+        var sum = 0
+        // 整形的 utf8 编码范围
+        let intRange = 48...57
+        // 小写 a~f 的 utf8 的编码范围
+        let lowercaseRange = 97...102
+        // 大写 A~F 的 utf8 的编码范围
+        let uppercasedRange = 65...70
+        for (index, c) in hexStr.utf8CString.enumerated() {
+            var intC = Int(c.byteSwapped)
+            if intC == 0 {
+                break
+            } else if intRange.contains(intC) {
+                intC -= 48
+            } else if lowercaseRange.contains(intC) {
+                intC -= 87
+            } else if uppercasedRange.contains(intC) {
+                intC -= 55
+            } else {
+                assertionFailure("输入字符串格式不对，每个字符都需要在0~9，a~f，A~F内")
+            }
+            sum = sum * 16 + intC
+            // 每两个十六进制字母代表8位，即一个字节
+            if index % 2 != 0 {
+                bytes.append(UInt8(sum))
+                sum = 0
+            }
+        }
+        return bytes
+    }
 }
 
 public struct ExtensionPQStringEncodable<T>: PQStringEncodable {
